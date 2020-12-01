71694f33e9ababaeeb42fb15d730cd1fbfd02148
Nikolai Zyulin <n.zyulin@carx-tech.com>
2020-12-01 16:44:38 +0300

    Auth fix

--- Assets/Scripts/Auth/AppGalleryService.cs
+++ Assets/Scripts/Auth/AppGalleryService.cs
@@ -18,40 +18,44 @@ namespace AuthServices
 		{
 			if (!m_initialized)
 			{
-				var authParams = new HuaweiIdAuthParamsHelper(HuaweiIdAuthParams.DEFAULT_AUTH_REQUEST_PARAM_GAME).SetIdToken().CreateParams();
+				Debug.Log("AppGalleryService -> CreateAuthParams");
+				var authParams = new HuaweiIdAuthParamsHelper(HuaweiIdAuthParams.DEFAULT_AUTH_REQUEST_PARAM_GAME).SetIdToken().SetAccessToken().CreateParams();
+				Debug.Log("AppGalleryService -> GetService");
 				m_authService = HuaweiIdAuthManager.GetService(authParams);
 
 				m_initialized = true;
-
 			}
 
+			Debug.Log("AppGalleryService -> Start SignOut");
 			var signOut = m_authService.SignOut();
 			signOut.AddOnSuccessListener(success =>
 			{
+				Debug.Log($"AppGalleryService -> SignOut Success");
 				SignIn(callback);
 			});
 			signOut.AddOnFailureListener(error =>
 			{
+				Debug.Log($"AppGalleryService -> SignOut Error {error.Message}");
 				SignIn(callback);
 			});
 		}
 
 		private void SignIn(Action<AuthServiceResult> callback)
 		{
-			var signIn = m_authService.SilentSignIn();
-			signIn.AddOnSuccessListener(authId =>
+			Debug.Log("AppGalleryService -> Start SignIn");
+			m_authService.StartSignIn(authId =>
 			{
 				TimeController.instance.CallFromMainThread(() =>
 				{
+					Debug.Log($"AppGalleryService -> SignIn Success {authId.DisplayName}");
 					m_isLoggedIn = true;
 					callback?.Invoke(new AuthServiceResult(true, authId.DisplayName));
 				});
-			});
-			signIn.AddOnFailureListener(error =>
+			}, error =>
 			{
 				TimeController.instance.CallFromMainThread(() =>
 				{
-					Debug.Log($"AppGalleryService -> Auth failed! {error.Message}!");
+					Debug.Log($"AppGalleryService -> SignIn Error {error.Message}");
 					callback?.Invoke(new AuthServiceResult(false));
 				});
 			});
@@ -59,12 +63,21 @@ namespace AuthServices
 
 		public void LogOut(Action<bool> callback)
 		{
-			if (isLoggedIn)
+			if (isLoggedIn && m_authService != null)
 			{
-				Debug.Log("AppGalleryService -> Performing sign out");
-				m_authService?.SignOut();
-				m_isLoggedIn = false;
-				callback?.Invoke(true);
+				Debug.Log("AppGalleryService -> Start SignOut");
+				var signOut = m_authService.SignOut();
+				signOut.AddOnSuccessListener(success =>
+				{
+					Debug.Log("AppGalleryService -> SignOut Success");
+					m_isLoggedIn = false;
+					callback?.Invoke(true);
+				});
+				signOut.AddOnFailureListener(error =>
+				{
+					Debug.Log($"AppGalleryService -> SignOut Error {error.Message}");
+					callback?.Invoke(false);
+				});
 			}
 			else
 			{
